a87758b2604cd8a3019c8844c7cdc2822d31104d
Gerard Eklu <gerard_eklu@homedepot.com>
2018-12-06 08:34:46 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Console Log
+console.log('Hey!')
+console.table({ firstName: 'Gerard', lastName: 'Dinho' })
+console.error('This is an error');
+console.warn('This is a warning');
+
+// JavaScript types
+// Primitive types : string, number, boolean, null, undefined, symbol.
+// Referencer types: arrays, objects literal, function, date
+// Date
+const today = new Date();
+console.log(today)
+
+
+
+// asm001 login
+// qa02Test! password
